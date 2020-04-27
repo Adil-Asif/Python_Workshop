1489affcce4a29de15133d4f5b382c4991a78122
Adil Asif <adilasif680@gmail.com>
2020-04-28 02:26:08 +0500

    Initial Commit

---
+++ Password_Generator.py
@@ -0,0 +1,10 @@
+import random
+
+char_set = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPPQRSTUVWXYZ123456789-_$£%&~/?|"
+
+length = input("Enter length of password: ")
+password = ''
+
+for i in range(0,int(length)):
+    password += random.choice(char_set)
+print(password)
